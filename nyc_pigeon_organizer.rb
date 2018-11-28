d15f2f6f96cf5847eb3f041d4a07a741fb9f1d9a
Rafael Cruz <ralph1786@gmail.com>
2018-11-28 17:23:42 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+
+  data.each do |attribute, options|
+    options.each do |option, pigeons_name|
+      pigeons_name.each do |pigeon_name|
+        new_hash[pigeon_name] ||= {}
+        new_hash[pigeon_name][attribute] ||= []
+        new_hash[pigeon_name][attribute] << option.to_s
+      end
+    end
+  end
+  new_hash
 end
